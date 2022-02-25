2ecdd6571d6266a103d821fc248ba732f4c0a47b
= <=>
2022-02-25 09:09:10 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused_imports)]
 
-us chess::{Board, MoveGen, Color, BoardStatus, ChessMove, ALL_RANKS, Piece, get_rank};
+use chess::{Board, MoveGen, Color, BoardStatus, ChessMove, ALL_RANKS, Piece, get_rank};
 use std::env;
 use std::io::{self, BufRead};
 use std::str::FromStr;
@@ -18,7 +18,7 @@ const DEFAULT_DEPTH: i64 = 7;
 const PROGRAM_DESC: &'static str = "A basic chess engine written in rust";
 const PROGRAM_NAME: &'static str = "Scythe";
 
-fn calc_piece_value(pc_idx: usize, sq_idx: usize, color: Option<Color> -> i64
+fn calc_piece_value(pc_idx: usize, sq_idx: usize, color: Option<Color>) -> i64 {
     match color {
         Some(Color::White) => {
             let sq_value = piece_values::PIECE_SQUARES[pc_idx][sq_idx];
@@ -34,6 +34,37 @@ fn calc_piece_value(pc_idx: usize, sq_idx: usize, color: Option<Color> -> i64
     }
 }
 
+fn calc_pieces_value(board: &Board) -> i64 {
+    let mut result = 0;
+    for pc_idx in 0..6 {
+        let pc_type = piece_values::PIECES[pc_idx];
+        let bboard = *board.pieces(pc_type);
+        for square in bboard {
+            let sq_idx = square.to_index();
+            result += calc_piece_value(pc_idx, sq_idx, board.color_on(square));
+        }
+    }
+    return result;
+}
+
+fn calc_board_value(board: &Board) -> i64 {
+    let w_move = board.side_to_move() == Color::White;
+    let result = match board.status() {
+        BoardStatus::Checkmate => if w_move {20000} else {-20000},
+        BoardStatus::Stalemate => 0,
+        BoardStatus::Ongoing => calc_pieces_values(board);
+    };
+    return result;
+}
+
+fn alpha_beta (
+    board: &Board, depth: i8,
+    is_max: bool, alpha: i64,
+    beta: i64, totatl: &mut i64) -> i64 {
+    if (depth == 0) || (board.status() != Board.Status::Ongoing) {
+        *total +  
+
+
 fn main() {
-    
+    println!("Hello");
 }
